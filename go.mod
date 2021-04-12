e0740f6f996b5740b64afca94258dc277b24206c
Densuke-fitness <densukefitness2020@gmail.com>
2021-04-12 21:19:46 +0900

    test ci

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Densuke-fitness/Practice4forGoCi
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Densuke-fitness/Practice4forGoCi/fizzbuzz"
+)
+
+func main() {
+	fmt.Println(fizzbuzz.Convert(15))
+}
